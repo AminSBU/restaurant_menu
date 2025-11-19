d323c0a40905f73d32442fe0adb8962e1f804544
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-11-19 22:38:25 +0330

    intro.jsx, 4 button for menu

---
+++ restaurant_menu/src/intro.jsx
@@ -0,0 +1,14 @@
+function Intro()
+{
+    return(
+        <>
+            <div className="intro-container">
+                <button className="restaurant-menu">منوی رستوران</button>
+                <button className="Cafe-menu">منوی کافه</button>
+                <button className="breakfast-menu">منوی صبحانه</button>
+                <button className="hooka-menu">منوی هوکا</button>
+            </div>
+        </>
+    );
+}
+export default Intro;
