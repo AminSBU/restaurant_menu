88ba6c911861bb881c6e79bcbfb52b70ed1d449e
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-11-21 09:34:22 +0330

    save menuItems base on useState

--- restaurant_menu/src/intro.jsx
+++ restaurant_menu/src/intro.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './intro.css'
 import Menu from './Menu.jsx'
 
 function Intro()
 {
+    const [menuItems, setMenuItems] = useState([]);
+
     useEffect(() => {
         fetch('/list.json')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                setMenuItems(data);
             })
             .catch(error => {   
                 console.error('Error fetching the menu:', error);
             });
     }, []);
-    
+
     return(
         <>
             <div className="intro-container">
