27e0194ca18514ac907580823e164b3c638400f1
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-11-22 21:45:21 +0330

    useState to save input values

--- restaurant_menu/src/NewPost.jsx
+++ restaurant_menu/src/NewPost.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
 function NewPost()
 {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState("");
+
     return(
         <>
             <div className="post_container">
                 <div className="post-title-div">
-                    <input type="text" className="post-title"></input>
+                    <input type="text" className="post-title" value={title}></input>
                 </div>
                 <div className="post-price-div">
-                    <input type="text" className="post-price"></input>
+                    <input type="text" className="post-price" value={price}></input>
                 </div>
                 <div className="post-description-div">
-                    <input type="text" className="post-description"></input>
+                    <input type="text" className="post-description" value={description}></input>
                 </div>
                 <div className='send-post'>
                     <button className='send-post-button'>send</button>
