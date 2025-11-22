aafe448677a04d05e13546cefbcd726d927c75c8
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-11-22 08:52:51 +0330

    set grid for css file, use json file in js

--- restaurant_menu/src/Menu.jsx
+++ restaurant_menu/src/Menu.jsx
@@ -1,10 +1,20 @@
 import './Menu.css'
 
-function Menu()
+function Menu({items})
 {
     return(
         <>
-            
+            <div className="menu-grid">
+                {items.map((item, index) => (
+                    <div key={index} className="menu-card">
+                    <h3 className="menu-title">{item.title}</h3>
+                    <p className="menu-description">{item.description}</p>
+                    <div className="menu-price">
+                        {item.price.toLocaleString('fa-IR')} تومان
+                    </div>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
--- restaurant_menu/src/intro.jsx
+++ restaurant_menu/src/intro.jsx
@@ -1,28 +1,15 @@
 import { useEffect, useState } from 'react';
 import './intro.css'
 import Menu from './Menu.jsx'
+import itemsList from './list.json';
 
 function Intro()
 {
-    const [menuItems, setMenuItems] = useState([]);
-
-    useEffect(() => {
-        fetch('/list.json')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setMenuItems(data);
-            })
-            .catch(error => {   
-                console.error('Error fetching the menu:', error);
-            });
-    }, []);
-
     return(
         <>
             <div className="intro-container">
-                <h1>منوی رستوران</h1>
-                <Menu />
+                
+                <Menu items={itemsList} />
             </div>
         </>
     );
