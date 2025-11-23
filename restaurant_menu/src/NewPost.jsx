3c6c0de9261076a627424ab2b89bff09e1a37902
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-11-23 19:04:24 +0330

    set handlechange to inputs

--- restaurant_menu/src/NewPost.jsx
+++ restaurant_menu/src/NewPost.jsx
@@ -6,17 +6,40 @@ function NewPost()
     const [price, setPrice] = useState(0);
     const [description, setDescription] = useState("");
 
+    const handlechange = (e) => {
+        const [value] = e.target;
+        set
+    }
+
     return(
         <>
             <div className="post_container">
                 <div className="post-title-div">
-                    <input type="text" className="post-title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                    <input 
+                        type="text" 
+                        className="post-title"
+                        name="post-title"
+                        value={title} 
+                        onChange={(e) => setTitle(e.target.value)} 
+                    />
                 </div>
                 <div className="post-price-div">
-                    <input type="text" className="post-price" value={price} onChange={(e) => setTitle(e.target.value)}></input>
+                    <input 
+                        type="text" 
+                        className="post-price" 
+                        name="post-price"
+                        value={price} 
+                        onChange={(e) => setTitle(e.target.value)} 
+                    />
                 </div>
                 <div className="post-description-div">
-                    <input type="text" className="post-description" value={description} onChange={(e) => setTitle(e.target.value)}></input>
+                    <input 
+                        type="text" 
+                        className="post-description" 
+                        name="post-description"
+                        value={description} 
+                        onChange={(e) => setTitle(e.target.value)} 
+                    />
                 </div>
                 <div className='send-post'>
                     <button className='send-post-button'>send</button>
