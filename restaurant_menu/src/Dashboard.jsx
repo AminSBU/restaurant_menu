13f25f764f843bf02de2eb093ababbdce6d4c92a
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-12-20 22:44:18 +0330

    sample code for dashboard menu added to project

--- restaurant_menu/src/Dashboard.jsx
+++ restaurant_menu/src/Dashboard.jsx
@@ -1,8 +1,19 @@
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
 function Dashboard()
 {
     return(
         <>
-            <h1>dashboard</h1>
+            <Sidebar>
+                <Menu>
+                    <SubMenu label="Charts">
+                    <MenuItem> Pie charts </MenuItem>
+                    <MenuItem> Line charts </MenuItem>
+                    </SubMenu>
+                    <MenuItem> Documentation </MenuItem>
+                    <MenuItem> Calendar </MenuItem>
+                </Menu>
+            </Sidebar>;
         </>
     );
 }
